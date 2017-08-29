cc4c225f44da3c3b3c68665db1c78c8e478ae23b
Jeff Hajewski <jeff.hajewski@gmail.com>
2017-08-28 20:10:41 -0500

    Adds two function signatures

--- hotfuzz.go
+++ hotfuzz.go
@@ -1,2 +1,10 @@
 // hotfuzz implementation
 package hotfuzz
+
+// RunCommand executes the given command and fuzzes the input
+func RunCommand(cmd string) {
+}
+
+// Generates a fuzz list with the given repeat index
+func generateFuzzList(repeat int) {
+}
